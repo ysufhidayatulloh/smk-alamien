df1d07fbcbb6dd303bae07f06c6ccfa1efadac23
ysufhidayatulloh <ysfhidayatulloh12@mail.com>
2024-03-01 19:51:59 +0700

    fix: img-activity3

--- public/utils.jsx
+++ public/utils.jsx
@@ -1,7 +1,7 @@
 import logo from "./images/logo_Navbar.png";
 import activity1 from "./images/ImageActivity-Img1.jpg";
 import activity2 from "./images/ImageActivity-Img2.jpg";
-import activity3 from "./images/ImageActivity-Img3.jpg";
+import activity3 from "./images/ImageActivity-Img.jpg";
 // import pamflate from "./images/Pamflate.png";
 // import herobg from "./images/HeroSection-image.jpg";
 
